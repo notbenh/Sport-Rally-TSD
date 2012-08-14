122b65dd9488702a859d9099a8850da68ac4d48a
ben hengst <ben.hengst@gmail.com>
2012-08-13 22:00:24 -0700

    more sketching

--- lib/Sport/Rally/TSD.pm
+++ lib/Sport/Rally/TSD.pm
@@ -1,7 +1,20 @@
-use strict;
-use warnings;
 package Sport::Rally::TSD;
+use Moose;
 # ABSTRACT: a way to have the computer do most of the math for you
-sub new{ return bless {}, shift };
+
+has route => is => 'rw'
+           , isa => 'Sport::Rally::TSD::Route'
+           , lazy => 1
+           , default => sub{ require Sport::Rally::TSD::Route; 
+                             Sport::Rally::TSD::Route->new;
+                           }
+;
+
+sub parse {
+  my $self = shift;
+  $self->route->from_file(shift);
+}
+
+
 
 1;
